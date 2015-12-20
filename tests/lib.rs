6cec45411190a5b99db32a59937ad3d2493de3d5
antklim <antklim@gmail.com>
2015-12-20 15:37:02 +1100

    DNA nucleotid calculator
    * DNA nucleotid calculator implementation

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,9 +1,18 @@
 extern crate rosalind;
 
+use rosalind::RosalindError::UnknownNucleotid;
+use rosalind::DNANucleotides;
 use rosalind::count_dna_nucleotides;
+
+#[test]
+fn it_should_return_error_when_unknown_nucleotid_found() {
+  let dna = "Z";
+  assert_eq!(count_dna_nucleotides(dna).unwrap_err(), UnknownNucleotid('Z'));
+}
 
 #[test]
 fn it_should_count_dna_nucleotides() {
   let dna = "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC";
-  assert_eq!("20 12 17 21", count_dna_nucleotides(dna));
+  let dna_nucleotides = DNANucleotides {A: 20, C: 12, G: 17, T: 21};
+  assert_eq!(count_dna_nucleotides(dna).unwrap(), dna_nucleotides);
 }
