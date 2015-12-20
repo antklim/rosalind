737b9e56c0f9bce0da28353ea0d84fb53471c17d
antklim <antklim@gmail.com>
2015-12-20 21:54:23 +1100

    Transcribing DNA into RNA implemented

--- tests/lib.rs
+++ tests/lib.rs
@@ -2,27 +2,45 @@ extern crate rosalind;
 
 use rosalind::RosalindError::UnknownNucleotide;
 use rosalind::dna::*;
+use rosalind::rna::*;
 
+// DNA =======================================================================
 #[test]
-fn it_should_return_error_when_unknown_nucleotid_found() {
-  let dna = "Z";
-  assert_eq!(count_dna_nucleotides(dna).unwrap_err(), UnknownNucleotide('Z'));
+fn dna_should_return_error_when_unknown_nucleotid_found() {
+  assert_eq!(count_dna_nucleotides("Z").unwrap_err(), UnknownNucleotide('Z'));
 }
 
 #[test]
-fn it_should_count_dna_nucleotides() {
+fn dna_should_count_dna_nucleotides() {
   let dna = "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC";
   let dna_nucleotides = DNANucleotides {A: 20, C: 12, G: 17, T: 21};
   assert_eq!(count_dna_nucleotides(dna).unwrap(), dna_nucleotides);
 }
 
 #[test]
-fn it_should_format_dna_nucleotides() {
+fn dna_should_format_dna_nucleotides() {
   let dna_nucleotides = DNANucleotides {A: 1, C: 2, G: 3, T: 4};
   assert_eq!(dna_nucleotides.to_string(), "1 2 3 4");
 }
 
 #[test]
-fn it_should_skip_new_line_symbol() {
+fn dna_should_skip_new_line_symbol() {
   assert_eq!(count_dna_nucleotides("\n").unwrap(), DNANucleotides {A: 0, C: 0, G: 0, T: 0});
+}
+
+// RNA =======================================================================
+#[test]
+fn rna_should_return_error_when_unknown_nucleotid_found() {
+  assert_eq!(transcribe_dna_into_rna("Z").unwrap_err(), UnknownNucleotide('Z'));
+}
+
+#[test]
+fn rna_should_transcribe_dna_into_rna() {
+  let dna = "GATGGAACTTGACTACGTAAATT";
+  assert_eq!(transcribe_dna_into_rna(dna).unwrap(), "GAUGGAACUUGACUACGUAAAUU");
+}
+
+#[test]
+fn rna_should_skip_new_line_symbol() {
+  assert_eq!(transcribe_dna_into_rna("\n").unwrap(), "");
 }
