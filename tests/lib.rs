8245afd160377bbd7d70ed470f2d1c8553914056
antklim <antklim@gmail.com>
2015-12-20 17:20:15 +1100

    DNANucleotides structure Display implementation added

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,13 +1,13 @@
 extern crate rosalind;
 
-use rosalind::RosalindError::UnknownNucleotid;
+use rosalind::RosalindError::UnknownNucleotide;
 use rosalind::DNANucleotides;
 use rosalind::count_dna_nucleotides;
 
 #[test]
 fn it_should_return_error_when_unknown_nucleotid_found() {
   let dna = "Z";
-  assert_eq!(count_dna_nucleotides(dna).unwrap_err(), UnknownNucleotid('Z'));
+  assert_eq!(count_dna_nucleotides(dna).unwrap_err(), UnknownNucleotide('Z'));
 }
 
 #[test]
@@ -15,4 +15,10 @@ fn it_should_count_dna_nucleotides() {
   let dna = "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC";
   let dna_nucleotides = DNANucleotides {A: 20, C: 12, G: 17, T: 21};
   assert_eq!(count_dna_nucleotides(dna).unwrap(), dna_nucleotides);
+}
+
+#[test]
+fn it_should_format_dna_nucleotides() {
+  let dna_nucleotides = DNANucleotides {A: 1, C: 2, G: 3, T: 4};
+  assert_eq!(dna_nucleotides.to_string(), "1 2 3 4");
 }
